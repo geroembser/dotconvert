5961bc0d2b5506d45d9fdc5cb657d5e5ce5225dc
Gero <gero.embser@gmail.com>
2024-10-20 07:49:00 +0200

    pod setup and ffmpeg

---
+++ Pods/Target Support Files/Pods-dotconvert-dotconvertUITests/Pods-dotconvert-dotconvertUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dotconvert_dotconvertUITests : NSObject
+@end
+@implementation PodsDummy_Pods_dotconvert_dotconvertUITests
+@end
---
+++ Pods/Target Support Files/Pods-dotconvert/Pods-dotconvert-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dotconvert : NSObject
+@end
+@implementation PodsDummy_Pods_dotconvert
+@end
